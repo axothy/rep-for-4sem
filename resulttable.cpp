463630b6ee501540d39220b986752e650088e107
Alexander Chebotin <61747868+axothy@users.noreply.github.com>
2021-05-19 12:40:33 +0300

    Delete resulttable.cpp

--- resulttable.cpp
+++
@@ -1,9 +0,0 @@
-#include "resulttable.h"
-
-void ResultTable::PrintResults() {
-	std::map <std::string, int> ::iterator it = reportExam.begin();
-	for (; it != reportExam.end(); it++)
-	{
-		std::cout << it->first << " " << it->second << std::endl;
-	}
-}
